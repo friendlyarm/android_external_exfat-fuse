3c14dce26a3caf43c1f2d373e6cde9bbd2a1d5ec
Lawrence Mok <lawrence.mok@amlogic.com>
2014-01-06 11:33:14 -0800

    remove trailing spaces to fix dependency install

--- Android.mk
+++ Android.mk
@@ -5,23 +5,23 @@ include $(CLEAR_VARS)
 
 SRCPATH := lib
 
-LOCAL_CFLAGS	:= -D_FILE_OFFSET_BITS=64 -DFUSE_USE_VERSION=26
+LOCAL_CFLAGS := -D_FILE_OFFSET_BITS=64 -DFUSE_USE_VERSION=26
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE    := libfuse_exfat
-LOCAL_SRC_FILES := $(SRCPATH)/cuse_lowlevel.c 
-LOCAL_SRC_FILES += $(SRCPATH)/fuse.c 
-LOCAL_SRC_FILES += $(SRCPATH)/fuse_kern_chan.c 
-LOCAL_SRC_FILES += $(SRCPATH)/fuse_loop.c 
-LOCAL_SRC_FILES += $(SRCPATH)/fuse_loop_mt.c 
-LOCAL_SRC_FILES += $(SRCPATH)/fuse_lowlevel.c 
-LOCAL_SRC_FILES += $(SRCPATH)/fuse_mt.c 
-LOCAL_SRC_FILES += $(SRCPATH)/fuse_opt.c 
-LOCAL_SRC_FILES += $(SRCPATH)/fuse_session.c 
-LOCAL_SRC_FILES += $(SRCPATH)/fuse_signals.c 
-LOCAL_SRC_FILES += $(SRCPATH)/helper.c 
-LOCAL_SRC_FILES += $(SRCPATH)/mount.c 
-LOCAL_SRC_FILES += $(SRCPATH)/mount_util.c 
+LOCAL_SRC_FILES := $(SRCPATH)/cuse_lowlevel.c
+LOCAL_SRC_FILES += $(SRCPATH)/fuse.c
+LOCAL_SRC_FILES += $(SRCPATH)/fuse_kern_chan.c
+LOCAL_SRC_FILES += $(SRCPATH)/fuse_loop.c
+LOCAL_SRC_FILES += $(SRCPATH)/fuse_loop_mt.c
+LOCAL_SRC_FILES += $(SRCPATH)/fuse_lowlevel.c
+LOCAL_SRC_FILES += $(SRCPATH)/fuse_mt.c
+LOCAL_SRC_FILES += $(SRCPATH)/fuse_opt.c
+LOCAL_SRC_FILES += $(SRCPATH)/fuse_session.c
+LOCAL_SRC_FILES += $(SRCPATH)/fuse_signals.c
+LOCAL_SRC_FILES += $(SRCPATH)/helper.c
+LOCAL_SRC_FILES += $(SRCPATH)/mount.c
+LOCAL_SRC_FILES += $(SRCPATH)/mount_util.c
 LOCAL_SRC_FILES += $(SRCPATH)/ulockmgr.c
 
 LOCAL_SHARED_LIBRARIES := libdl
@@ -34,18 +34,18 @@ include $(CLEAR_VARS)
 
 SRCPATH := libexfat
 
-LOCAL_CFLAGS	:= -D_FILE_OFFSET_BITS=64 -DFUSE_USE_VERSION=26  -D__GLIBC__=1
+LOCAL_CFLAGS := -D_FILE_OFFSET_BITS=64 -DFUSE_USE_VERSION=26  -D__GLIBC__=1
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE    := libexfat
-LOCAL_SRC_FILES := $(SRCPATH)/cluster.c 
-LOCAL_SRC_FILES += $(SRCPATH)/io.c 
-LOCAL_SRC_FILES += $(SRCPATH)/log.c 
-LOCAL_SRC_FILES += $(SRCPATH)/lookup.c 
-LOCAL_SRC_FILES += $(SRCPATH)/mount.c 
-LOCAL_SRC_FILES += $(SRCPATH)/node.c 
-LOCAL_SRC_FILES += $(SRCPATH)/utf.c 
-LOCAL_SRC_FILES += $(SRCPATH)/utils.c 
+LOCAL_SRC_FILES := $(SRCPATH)/cluster.c
+LOCAL_SRC_FILES += $(SRCPATH)/io.c
+LOCAL_SRC_FILES += $(SRCPATH)/log.c
+LOCAL_SRC_FILES += $(SRCPATH)/lookup.c
+LOCAL_SRC_FILES += $(SRCPATH)/mount.c
+LOCAL_SRC_FILES += $(SRCPATH)/node.c
+LOCAL_SRC_FILES += $(SRCPATH)/utf.c
+LOCAL_SRC_FILES += $(SRCPATH)/utils.c
 
 LOCAL_SHARED_LIBRARIES := libdl libfuse_exfat
 LOCAL_PRELINK_MODULE := false
@@ -55,7 +55,7 @@ include $(CLEAR_VARS)
 
 LOCAL_CFLAGS    := -D_FILE_OFFSET_BITS=64 -D__GLIBC__=1
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/libexfat
-LOCAL_MODULE    := dumpexfat 
+LOCAL_MODULE    := dumpexfat
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := dump/main.c
 LOCAL_SHARED_LIBRARIES := libexfat
@@ -65,7 +65,7 @@ include $(CLEAR_VARS)
 
 LOCAL_CFLAGS    := -D_FILE_OFFSET_BITS=64 -D__GLIBC__=1
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/libexfat
-LOCAL_MODULE    := fsck.exfat 
+LOCAL_MODULE    := fsck.exfat
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := fsck/main.c
 LOCAL_SHARED_LIBRARIES := libexfat
@@ -75,7 +75,7 @@ include $(CLEAR_VARS)
 
 LOCAL_CFLAGS    := -D_FILE_OFFSET_BITS=64 -D__GLIBC__=1
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/libexfat
-LOCAL_MODULE    := exfat.label 
+LOCAL_MODULE    := exfat.label
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := label/main.c
 LOCAL_SHARED_LIBRARIES := libexfat
@@ -85,7 +85,7 @@ include $(CLEAR_VARS)
 
 LOCAL_CFLAGS    := -D_FILE_OFFSET_BITS=64 -D__GLIBC__=1
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/libexfat
-LOCAL_MODULE    := mkfs.exfat 
+LOCAL_MODULE    := mkfs.exfat
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := mkfs/main.c mkfs/cbm.c mkfs/fat.c mkfs/rootdir.c mkfs/uct.c mkfs/vbr.c
 LOCAL_SHARED_LIBRARIES := libexfat
@@ -95,7 +95,7 @@ include $(CLEAR_VARS)
 
 LOCAL_CFLAGS    := -D_FILE_OFFSET_BITS=64 -DFUSE_USE_VERSION=26 -D__GLIBC__=1
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(LOCAL_PATH)/libexfat
-LOCAL_MODULE    := mount.exfat 
+LOCAL_MODULE    := mount.exfat
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := fuse/main.c
 LOCAL_SHARED_LIBRARIES := libfuse_exfat libexfat
